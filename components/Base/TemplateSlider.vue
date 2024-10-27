dc0147b91745673c5247d7eadb9d8b4003a9b124
navidjaberi <dkpnavid@gmail.com>
2024-10-28 02:45:16 +0330

    add template screenshots

---
+++ components/Base/TemplateSlider.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-slide-group
+    v-model="model"
+    class="ltr mt-5"
+    selected-class="bg-primary"
+    multiple
+    show-arrows
+    prev-icon="mdi-chevron-left-circle-outline"
+    next-icon="mdi-chevron-right-circle-outline"
+  >
+    <CardTemplate v-for="n in 15" :key="n" />
+  </v-slide-group>
+</template>
+
+<script setup>
+const model = ref();
+const templates = ref([
+  {
+    name: "fantasy",
+    modes: [
+      {
+        name: "blue",
+        imgs: [
+          "/templates/fantasy/Blue/Home.png",
+          "/templates/fantasy/Blue/Menu.png",
+          "/templates/fantasy/Blue/Mini-Site.png",
+        ],
+        color: "",
+      },
+      {
+        name: "gold",
+        imgs: [
+          "/templates/fantasy/Gold/Home.png",
+          "/templates/fantasy/Gold/Menu.png",
+          "/templates/fantasy/Gold/Mini-Site.png",
+        ],
+        color: "",
+      },
+      {
+        name: "mint",
+        imgs: [
+          "/templates/fantasy/Mint/Home.png",
+          "/templates/fantasy/Mint/Menu.png",
+          "/templates/fantasy/Mint/Mini-Site.png",
+        ],
+        color: "",
+      },
+      {
+        name: "pink",
+        imgs: [
+          "/templates/fantasy/Pink/Home.png",
+          "/templates/fantasy/Pink/Menu.png",
+          "/templates/fantasy/Pink/Mini-Site.png",
+        ],
+        color: "",
+      },
+      {
+        name: "purple",
+        imgs: [
+          "/templates/fantasy/Purple/Home.png",
+          "/templates/fantasy/Purple/Menu.png",
+          "/templates/fantasy/Purple/Mini-Site.png",
+        ],
+        color: "",
+      },
+    ],
+  },
+]);
+</script>
+
+<style></style>
